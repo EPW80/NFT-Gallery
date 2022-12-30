b7ab0c298b76ce04e6391abf624872089d720ef6
EWP80 <erikpw009@gmail.com>
2022-12-30 13:46:41 -0800

    initial commit

--- pages/components/nftCard.jsx
+++ pages/components/nftCard.jsx
@@ -1,27 +1,39 @@
 export const NFTCard = ({ nft }) => {
-
-    return (
-        <div className="w-1/4 flex flex-col ">
-        <div className="rounded-md">
-            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+  return (
+    <div className="flex w-1/4 flex-col ">
+      <div className="rounded-md">
+        <img
+          className="h-128 w-full rounded-t-md object-cover"
+          src={nft.media[0].gateway}
+        ></img>
+      </div>
+      <div className="y-gap-2 h-110 flex flex-col rounded-b-md px-2 py-3 ">
+        <div className="">
+          <h2 className="text-xl text-gray-800">{nft.title}</h2>
+          <p className="text-gray-600">
+            Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}
+          </p>
+          <p className="text-gray-600">{`${nft.contract.address.substr(
+            0,
+            4
+          )}...${nft.contract.address.substr(
+            nft.contract.address.length - 4
+          )}`}</p>
         </div>
-        <div className="flex flex-col y-gap-2 px-2 py-3 rounded-b-md h-110 ">
-            <div className="">
-                <h2 className="text-xl text-gray-800">{nft.title}</h2>
-                <p className="text-gray-600">Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
-                <p className="text-gray-600" >{`${nft.contract.address.substr(0, 4)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
-            </div>
 
-            <div className="flex-grow mt-2">
-                <p className="text-gray-600">{nft.description?.substr(0, 150)}</p>
-            </div>
-            <div className="flex justify-center mb-1">
-                <a target={"_blank"} href={`https://etherscan.io/token/${nft.contract.address}`} className="py-2 px-4 bg-blue-100 w-1/2 text-center rounded-m text- cursor-pointer">View on etherscan</a>
-            </div>
+        <div className="mt-2 flex-grow">
+          <p className="text-gray-600">{nft.description?.substr(0, 150)}</p>
         </div>
-
+        <div className="mb-1 flex justify-center">
+          <a
+            target={'_blank'}
+            href={`https://etherscan.io/token/${nft.contract.address}`}
+            className="rounded-m text- w-1/2 cursor-pointer bg-blue-100 py-2 px-4 text-center"
+          >
+            View on etherscan
+          </a>
+        </div>
+      </div>
     </div>
-    )
-   
+  )
 }
-export default NFTCard
